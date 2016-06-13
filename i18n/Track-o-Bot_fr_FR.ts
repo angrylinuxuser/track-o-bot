45be22dd6ab3f1c1414b2db7360a9c075c01c829
cbosselut <bossone0013@gmail.com>
2016-06-13 08:58:36 +0200

    Update french translation

--- i18n/Track-o-Bot_fr_FR.ts
+++ i18n/Track-o-Bot_fr_FR.ts
@@ -4,12 +4,12 @@
 <context>
     <name>AboutWidget</name>
     <message>
-        <location filename="src/ui/AboutWidget.ui" line="20"/>
+        <location filename="../src/ui/AboutWidget.ui" line="20"/>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="src/ui/AboutWidget.ui" line="38"/>
+        <location filename="../src/ui/AboutWidget.ui" line="38"/>
         <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
 &lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
 p, li { white-space: pre-wrap; }
@@ -21,30 +21,30 @@ p, li { white-space: pre-wrap; }
 &lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Arial&apos;; font-size:12px; font-weight:600;&quot;&gt;VERSION&lt;/span&gt;&lt;/p&gt;
 &lt;p align=&quot;center&quot; style=&quot;-qt-paragraph-type:empxy; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Arial&apos;; font-size:8px;&quot;&gt; &lt;/p&gt;
 &lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;a href=&quot;https://github.com/stevschmid/track-o-bot&quot;&gt;&lt;span style=&quot; font-family:&apos;Arial&apos;; font-size:12px; text-decoration: underline; color:#0000ff;&quot;&gt;GitHub&lt;/span&gt;&lt;/a&gt;&lt;span style=&quot; font-family:&apos;Arial&apos;; font-size:12px;&quot;&gt; | &lt;/span&gt;&lt;a href=&quot;https://www.reddit.com/r/trackobot&quot;&gt;&lt;span style=&quot; font-family:&apos;Arial&apos;; font-size:12px; text-decoration: underline; color:#0000ff;&quot;&gt;Reddit&lt;/span&gt;&lt;/a&gt;&lt;span style=&quot; font-family:&apos;Arial&apos;; font-size:12px;&quot;&gt; | &lt;/span&gt;&lt;a href=&quot;https://twitter.com/trackobot&quot;&gt;&lt;span style=&quot; font-family:&apos;Arial&apos;; font-size:12px; text-decoration: underline; color:#0000ff;&quot;&gt;Twitter&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 <context>
     <name>AccountTab</name>
     <message>
-        <location filename="src/ui/AccountTab.cpp" line="37"/>
+        <location filename="../src/ui/AccountTab.cpp" line="37"/>
         <source>Export Track-o-Bot Account Data</source>
         <translation>Exporter les données du compte Track-o-Bot</translation>
     </message>
     <message>
-        <location filename="src/ui/AccountTab.cpp" line="38"/>
-        <location filename="src/ui/AccountTab.cpp" line="64"/>
+        <location filename="../src/ui/AccountTab.cpp" line="38"/>
+        <location filename="../src/ui/AccountTab.cpp" line="64"/>
         <source>Account Data (*.track-o-bot);; All Files (*)</source>
         <translation>Donnée du compte (*.track-o-bot);; Tous les fichiers (*)</translation>
     </message>
     <message>
-        <location filename="src/ui/AccountTab.cpp" line="45"/>
-        <location filename="src/ui/AccountTab.cpp" line="71"/>
+        <location filename="../src/ui/AccountTab.cpp" line="45"/>
+        <location filename="../src/ui/AccountTab.cpp" line="71"/>
         <source>Unable to open file</source>
         <translation>Impossible d&apos;ouvrir le fichier</translation>
     </message>
     <message>
-        <location filename="src/ui/AccountTab.cpp" line="63"/>
+        <location filename="../src/ui/AccountTab.cpp" line="63"/>
         <source>Import Track-o-Bot Account Data</source>
         <translation>Importer un compte Track-o-Bot</translation>
     </message>
@@ -52,37 +52,37 @@ p, li { white-space: pre-wrap; }
 <context>
     <name>AccountWidget</name>
     <message>
-        <location filename="src/ui/AccountWidget.ui" line="20"/>
+        <location filename="../src/ui/AccountWidget.ui" line="20"/>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="src/ui/AccountWidget.ui" line="59"/>
+        <location filename="../src/ui/AccountWidget.ui" line="59"/>
         <source>Account</source>
         <translation>Compte</translation>
     </message>
     <message>
-        <location filename="src/ui/AccountWidget.ui" line="72"/>
+        <location filename="../src/ui/AccountWidget.ui" line="72"/>
         <source>None</source>
         <translation>Aucun</translation>
     </message>
     <message>
-        <location filename="src/ui/AccountWidget.ui" line="93"/>
+        <location filename="../src/ui/AccountWidget.ui" line="93"/>
         <source>Export...</source>
         <translation>Exporter...</translation>
     </message>
     <message>
-        <location filename="src/ui/AccountWidget.ui" line="103"/>
+        <location filename="../src/ui/AccountWidget.ui" line="103"/>
         <source>Import...</source>
         <translation>Importer...</translation>
     </message>
     <message>
-        <location filename="src/ui/AccountWidget.ui" line="115"/>
+        <location filename="../src/ui/AccountWidget.ui" line="115"/>
         <source>Use export to back up your account or to import it on another device. You can use the account on multiple devices simultaneously. </source>
         <translation>Utilisez l&apos;exportation pour effectuer une sauvegarde de votre compte ou afin de l&apos;importer sur un autre terminal. Vous pouvez utiliser le même compte sur plusieurs terminaux simultanément.</translation>
     </message>
     <message>
-        <location filename="src/ui/AccountWidget.ui" line="125"/>
+        <location filename="../src/ui/AccountWidget.ui" line="125"/>
         <source>The file contains only your account credentials. The results are stored online!</source>
         <translation>Le fichier contient vos identifiants. Vos résultats sont stockés en ligne !</translation>
     </message>
@@ -90,43 +90,43 @@ p, li { white-space: pre-wrap; }
 <context>
     <name>LogWidget</name>
     <message>
-        <location filename="src/ui/LogWidget.ui" line="20"/>
+        <location filename="../src/ui/LogWidget.ui" line="20"/>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="src/ui/MainWindow.ui" line="20"/>
+        <location filename="../src/ui/MainWindow.ui" line="20"/>
         <source>MainWindow</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="src/ui/MainWindow.ui" line="51"/>
+        <location filename="../src/ui/MainWindow.ui" line="51"/>
         <source>toolBar</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="src/ui/MainWindow.ui" line="82"/>
-        <location filename="src/ui/MainWindow.ui" line="85"/>
+        <location filename="../src/ui/MainWindow.ui" line="82"/>
+        <location filename="../src/ui/MainWindow.ui" line="85"/>
         <source>Settings</source>
         <translation>Paramètres</translation>
     </message>
     <message>
-        <location filename="src/ui/MainWindow.ui" line="97"/>
+        <location filename="../src/ui/MainWindow.ui" line="97"/>
         <source>Log</source>
         <translation>Journaux</translation>
     </message>
     <message>
-        <location filename="src/ui/MainWindow.ui" line="109"/>
-        <location filename="src/ui/MainWindow.ui" line="112"/>
+        <location filename="../src/ui/MainWindow.ui" line="109"/>
+        <location filename="../src/ui/MainWindow.ui" line="112"/>
         <source>About</source>
         <translation>À propos</translation>
     </message>
     <message>
-        <location filename="src/ui/MainWindow.ui" line="124"/>
-        <location filename="src/ui/MainWindow.ui" line="127"/>
+        <location filename="../src/ui/MainWindow.ui" line="124"/>
+        <location filename="../src/ui/MainWindow.ui" line="127"/>
         <source>Account</source>
         <translation>Compte</translation>
     </message>
@@ -134,98 +134,115 @@ p, li { white-space: pre-wrap; }
 <context>
     <name>Overlay</name>
     <message>
-        <location filename="src/ui/Overlay.ui" line="14"/>
+        <location filename="../src/ui/Overlay.ui" line="14"/>
         <source>Overlay</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 <context>
     <name>SettingsTab</name>
     <message>
-        <location filename="src/ui/SettingsTab.cpp" line="66"/>
+        <location filename="../src/ui/SettingsTab.cpp" line="70"/>
         <source>Select Hearthstone directory</source>
         <translation>Répertoire d&apos;installation d&apos;Hearstone</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/SettingsTab.cpp" line="88"/>
+        <source>Select wine prefix directory</source>
+        <translation>Préfixe Wine</translation>
     </message>
 </context>
 <context>
     <name>SettingsWidget</name>
     <message>
-        <location filename="src/ui/SettingsWidget.ui" line="20"/>
+        <location filename="../src/ui/SettingsWidget.ui" line="20"/>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="src/ui/SettingsWidget.ui" line="56"/>
+        <location filename="../src/ui/SettingsWidget.ui" line="137"/>
         <source>Game</source>
         <translation>Jeu</translation>
     </message>
     <message>
-        <location filename="src/ui/SettingsWidget.ui" line="79"/>
+        <location filename="../src/ui/SettingsWidget.ui" line="80"/>
+        <location filename="../src/ui/SettingsWidget.ui" line="160"/>
         <source>Select path...</source>
         <translation>Choisir le chemin...</translation>
     </message>
     <message>
-        <location filename="src/ui/SettingsWidget.ui" line="86"/>
+        <location filename="../src/ui/SettingsWidget.ui" line="167"/>
         <source>This should point to the Hearthstone directory.</source>
         <translation>Doit pointer vers le répertoire d&apos;installation d&apos;Hearthstone.</translation>
     </message>
     <message>
-        <location filename="src/ui/SettingsWidget.ui" line="109"/>
+        <location filename="../src/ui/SettingsWidget.ui" line="110"/>
+        <location filename="../src/ui/SettingsWidget.ui" line="190"/>
         <source>...</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="src/ui/SettingsWidget.ui" line="136"/>
+        <location filename="../src/ui/SettingsWidget.ui" line="57"/>
+        <source>Wine</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/SettingsWidget.ui" line="87"/>
+        <source>This should point to the Wine prefix directory.</source>
+        <translation>Doit pointer vers le dossier de préfixe Wine.</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/SettingsWidget.ui" line="217"/>
         <source>Enable in-game overlay</source>
         <translation>Afficher les données complémentaires en jeu</translation>
     </message>
     <message>
-        <location filename="src/ui/SettingsWidget.ui" line="159"/>
+        <location filename="../src/ui/SettingsWidget.ui" line="250"/>
         <source>System</source>
         <translation>Système</translation>
     </message>
     <message>
-        <location filename="src/ui/SettingsWidget.ui" line="166"/>
+        <location filename="../src/ui/SettingsWidget.ui" line="257"/>
         <source>Start at login </source>
         <translation>Démarrer automatiquement</translation>
     </message>
     <message>
-        <location filename="src/ui/SettingsWidget.ui" line="173"/>
+        <location filename="../src/ui/SettingsWidget.ui" line="264"/>
         <source>Open Track-o-Bot when you log in.</source>
         <translation>Lancer Track-o-Bot à l&apos;ouverture de session.</translation>
     </message>
     <message>
-        <location filename="src/ui/SettingsWidget.ui" line="196"/>
+        <location filename="../src/ui/SettingsWidget.ui" line="287"/>
         <source>Check for updates</source>
         <translation>Rechercher les mises à jour</translation>
     </message>
     <message>
-        <location filename="src/ui/SettingsWidget.ui" line="203"/>
+        <location filename="../src/ui/SettingsWidget.ui" line="294"/>
         <source>Check for updates automatically.</source>
         <translation>Rechercher les mises à jour automatiquement.</translation>
     </message>
     <message>
-        <location filename="src/ui/SettingsWidget.ui" line="225"/>
+        <location filename="../src/ui/SettingsWidget.ui" line="316"/>
         <source>Check for updates now</source>
         <translation>Rechercher les mises à jour mantenant</translation>
     </message>
     <message>
-        <location filename="src/ui/SettingsWidget.ui" line="251"/>
+        <location filename="../src/ui/SettingsWidget.ui" line="342"/>
         <source>Debug</source>
         <translation>Débogage</translation>
     </message>
     <message>
-        <location filename="src/ui/SettingsWidget.ui" line="258"/>
+        <location filename="../src/ui/SettingsWidget.ui" line="349"/>
         <source>Enable debug mode</source>
         <translation>Activer le débogage</translation>
     </message>
     <message>
-        <location filename="src/ui/SettingsWidget.ui" line="265"/>
+        <location filename="../src/ui/SettingsWidget.ui" line="356"/>
         <source>Debug mode increases verbosity of output in the log tab and sends diagnostic data.</source>
         <translation>Le mode débogage permet d&apos;afficher plus d&apos;informations dans les journaux et d&apos;envoyer des données de diagnostic.</translation>
     </message>
     <message>
-        <location filename="src/ui/SettingsWidget.ui" line="275"/>
+        <location filename="../src/ui/SettingsWidget.ui" line="224"/>
         <source>Hover over your or your opponent&apos;s deck to see which cards were drawn or played.</source>
         <translation>Survollez votre pioche ainsi que celle de votre adversaire pour afficher la liste des cartes piochées ou jouées.</translation>
     </message>
@@ -233,22 +250,22 @@ p, li { white-space: pre-wrap; }
 <context>
     <name>Window</name>
     <message>
-        <location filename="src/ui/Window.cpp" line="128"/>
+        <location filename="../src/ui/Window.cpp" line="128"/>
         <source>Open Profile...</source>
         <translation>Mon Profil...</translation>
     </message>
     <message>
-        <location filename="src/ui/Window.cpp" line="131"/>
+        <location filename="../src/ui/Window.cpp" line="131"/>
         <source>Settings...</source>
         <translation>Paramètres...</translation>
     </message>
     <message>
-        <location filename="src/ui/Window.cpp" line="134"/>
+        <location filename="../src/ui/Window.cpp" line="134"/>
         <source>Quit</source>
         <translation>Quitter</translation>
     </message>
     <message>
-        <location filename="src/ui/Window.cpp" line="137"/>
+        <location filename="../src/ui/Window.cpp" line="137"/>
         <source>Please restart Hearthstone!</source>
         <translation>Merci de relancer Hearthstone!</translation>
     </message>
